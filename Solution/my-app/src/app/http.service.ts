637ef3d4c7a4163162c54dde004ab9a769977466
humpyreddy96 <hp411819@ohio.edu>
2021-09-28 12:22:04 +0000

    Initial Commit

---
+++ Solution/my-app/src/app/http.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/users');
+  }
+}
